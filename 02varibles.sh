b6a2b749817be51e0333621103eee6d1f531395b
renuchandu123 <venkatarenuka.6@gmail.com>
2024-04-25 19:27:06 +0530

    01-variables.sh

--- 02varibles.sh
+++ 02varibles.sh
@@ -4,5 +4,5 @@ $PERSON1=renuka  #no space between = and value
 $PERSON2=vani
 
 
-echo "$PERSON1 :: hi $PERSON2 how r u"
-echo "$PERSON2 ::i am good also working"
+echo "$PERSON1:: hi $PERSON2 how r u"
+echo "$PERSON2:: i am good also working"
