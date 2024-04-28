587c1ae6d2073296c4a2cce3a40147e132c33d85
renuchandu123 <venkatarenuka.6@gmail.com>
2024-04-28 12:47:28 +0530

    03-variables.sh

---
+++ 03-variable.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+PERSON1=SIRI   no space between = and value
+PERSON2=vani
+
+echo "$PERSON1:: Hi $PERSON2 i am good" 
+echo "$PERSON2:: Hi $PERSON1 A always good"
+echo "$PERSON1:: I am  learning  devops "
