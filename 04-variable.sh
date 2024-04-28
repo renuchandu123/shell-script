89951816ce6eff3c83f90c2d759179bc6ac45d25
renuchandu123 <venkatarenuka.6@gmail.com>
2024-04-28 13:12:38 +0530

    03-variables.sh

---
+++ 04-variable.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+
+
+echo "$PERSON1:: Hi $PERSON2 I am good "
+echo "$PERSON2:: Hi $PERSON1 I am learning devops  how do you ur work"
+echo "$PERSON1:: Ya good "
