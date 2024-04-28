0f65564981597847ff414417e0128c50b223ed52
renuchandu123 <venkatarenuka.6@gmail.com>
2024-04-28 12:10:14 +0530

    03-variables.sh

--- 01- variables.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash/
-
-PERSON1=ramesh
-PERSON2=suresh
-
-
-echo "$PERSON1:: hi $PERSON1,how r u".
-echo "$PERSON2:: hi $PERSON2,i am fine".
-echo "$PERSON1:: how was the work".
--- 02varibles.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-$PERSON1=renuka  #no space between = and value
-$PERSON2=vani
-
-
-echo "$PERSON1:: hi $PERSON2 how r u"
-echo "$PERSON2:: i am good also working"
