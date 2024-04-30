c05fc48a228db4d4130736a672d769b321b95fc7
renuchandu123 <venkatarenuka.6@gmail.com>
2024-04-30 15:16:30 +0530

    05-variable.sh

---
+++ 08-special -var.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo   "all variabls: $@"
+echo   "number of variables passed: $#"
+echo   "script name: $0"
+echo  " current working directory: $pwd"
+echo  "home directory of current user: $HOME"
+echo  "which user is running this script: $user
+echo  "hostname: $HOSTNAME"
+echo  "process ID of the current shell script:  $$"
+sleep 60
+echo "process ID of the last background command: $!"
