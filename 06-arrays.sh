efa1113e8b0d602f76d281dda7522e2d2213d949
renuchandu123 <venkatarenuka.6@gmail.com>
2024-04-30 12:36:16 +0530

    05-variable.sh

--- 06-arrays.sh
+++ 06-arrays.sh
@@ -9,4 +9,4 @@ MOVIES=("RRR" "Djtillu" "murari")
 
 echo "first movie is:  ${movies[0]}"
 echo "first movie is:  ${movies[1]}"
-echo "first movie is:  ${movies[2]}"
+echo "first movie is:  ${movies[@]}"
