9006148ca762ad9f018a31429e693bc5b31cfd2b
renuchandu123 <venkatarenuka.6@gmail.com>
2024-04-30 17:10:30 +0530

    05-variable.sh

--- 10-installation.sh
+++ 10-installation.sh
@@ -2,9 +2,12 @@
 
 USERID=$(id -u)
 if [$USERID -ne 0]
+ 
 then
+ 
  echo "please run this script with root access"
  else
-  echo "you are super user"
+  
+ echo "you are super user"
  fi
  dnf install mysql -y
