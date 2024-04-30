8dd467d8a3506019e0eba9a37cd490bc3c5bed8c
renuchandu123 <venkatarenuka.6@gmail.com>
2024-04-30 12:07:27 +0530

    05-variable.sh

---
+++ 06-arrays.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+MOVIES=("RRR" "Djtillu" "murari")
+# size of above array is 3
+# index are 0,1,2
+# list always starts with 0
+
+echo "first movie is :${ movies[0]}"
+echo "first movie is :${ movies[1]}"
+echo "first movie is :${ movies[@]}"
