cc978891b5f369d52af4ae552b7fa81ecbf32435
renuchandu123 <venkatarenuka.6@gmail.com>
2024-05-01 19:06:18 +0530

    10-installation.sh

--- 10-installation.sh
+++ 10-installation.sh
@@ -6,11 +6,30 @@ if [$USERID -ne 0]
 then
  
  echo "please run this script with root access"
+       exit.1 # manually exit if error comes 
  else
   
- echo "you are super user"
+     echo "you are super user"
  fi
   
- dnf install mysql -y
+      dnf install mysql -y
+
+ if[ $?  -ne 0 ]
+ then
+      echo "installation of my sql ....FAILURE"
+       exit.1
+else
+       echo "installation of my sql .....success"
+fi
+       dnf install git -y
+
+  if[ $?  ne -0]
+then
+       echo "installation of git........ FAILURE"
+        exit.1
+  else
+        echo "installation of git.....success"
+
+
   
- echo "is script proceeding"?
+         echo "is script proceeding"?
