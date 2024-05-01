23326b015c0e3d1d75e52e1081b021af068752b3
renuchandu123 <venkatarenuka.6@gmail.com>
2024-05-01 22:56:21 +0530

    10-installation.sh

--- 11-functions.sh
+++ 11-functions.sh
@@ -10,7 +10,7 @@
 }
 if     [ $USERID   -ne 0 ]
 then
-      please  "run this script with root access."
+      please "run this script with root access."
       exit 1 # manually  exit if error comes.
       else
 
