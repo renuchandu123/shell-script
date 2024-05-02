361ccc202871ca501fbae813b2dd86d8dd01799e
renuchandu123 <venkatarenuka.6@gmail.com>
2024-05-02 15:01:46 +0530

    11-functions.sh

--- 11-functions.sh
+++ 11-functions.sh
@@ -4,13 +4,18 @@ USERID=$(id -u)
 TIMESTAMP=$(date +%F-%H-%M-%S)
 SCRIPT-NAME=$(echo $0 | cut -d "." -f1)
 LOGILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+
+echo "script started executing at:$TIMESTAMP"
 
 VALIDATE (){
     if     [ $1  -ne 0 ]
     then
-         echo "$2......FAILURE"
+         echo "$2...... $R FAILURE" $N
     else
-          echo "$2.....SUCCESS"
+          echo "$2..... $G SUCCESS"
        
      fi
 
@@ -23,11 +28,14 @@ then
 else
         echo "you are  super user ."
 fi
-      dnf install my sql -y
+      dnf install my sql -y &>>$LOGFILE
       VALIDATE  $? "installing mySQL" 
 
-      dnf install  git -y
+      dnf install  git -y &>>$LOGFILE
       VALIDATE $?  "installing mysql"
+
+      dnf install docker -y &>>$LOGFILE
+      VALIDATE $? "installing docker"
 
  
 
