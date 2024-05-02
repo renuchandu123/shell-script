7b9791ecddcf05d7aa0b04f386b902cad50f87e2
renuchandu123 <venkatarenuka.6@gmail.com>
2024-05-02 13:09:29 +0530

    11-functions.sh

--- 11-functions.sh
+++ 11-functions.sh
@@ -1,21 +1,28 @@
 #!/bin/bash
 
+USERID=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+SCRIPT-NAME=$(echo $0 | cut -d "." -f1)
+LOGILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
 
-    USERID=$(id -u)
-     VALIDATE (){
+VALIDATE (){
+    if     [ $1  -ne 0 ]
+    then
+         echo "$2......FAILURE"
+    else
+          echo "$2.....SUCCESS"
+       
+     fi
 
-       echo "exit status:  $1"
-        echo "what are you doing: $2"
-    
-     }
-     if     [ $USERID   -ne 0 ]
-     then
+}
+
+if  [$USERID  -ne 0]
+then
       please "run this script with root access."
       exit 1 # manually  exit if error comes.
-      else
-
-     echo "you are  super user ."
-     fi
+else
+        echo "you are  super user ."
+fi
       dnf install my sql -y
       VALIDATE  $? "installing mySQL" 
 
