b95d5685392da33b7abdceeda560b8b72d7b68a8
renuchandu123 <venkatarenuka.6@gmail.com>
2024-05-03 20:43:43 +0530

    15-current-script.sh

--- 18-delete-old-logs.sh
+++ 18-delete-old-logs.sh
@@ -6,11 +6,11 @@ G="\e[32M"
 Y="\e[33m"
 N="\e[0m"
 
-if    [- d $SOURCE_DIRECTORY]
+if [-d $SOURCE_DIRECTORY]
 then
 
-    echo "$G source directory exists $N"
+    echo -e "$G source directory exists $N"
 else
-    echo "$R please make sure $SOURCE_DIRECTORY exists $N"
+    echo -e "$R please make sure $SOURCE_DIRECTORY exists $N"
     exit 1
 fi
